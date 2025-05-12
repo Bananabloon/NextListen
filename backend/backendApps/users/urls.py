c4c5b153cd4f3d677ee8b1bd21b71e4f8bea2701
Bananabloon <99277409+Bananabloon@users.noreply.github.com>
2025-05-12 09:39:32 +0200

    Revert "Backend"

--- backend/backendApps/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import RegisterView, MeView, PreferenceVectorView
-
-urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('me/', MeView.as_view(), name='me'),
-    path('preference-vector/', PreferenceVectorView.as_view(), name='preference-vector'),
-]
