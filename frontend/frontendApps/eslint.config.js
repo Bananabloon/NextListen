27dcafe285c93b07d0b3a1c28fa3921bbf5a2cf0
TeKayy <th3tk.idk@gmail.com>
2025-05-26 12:32:19 +0200

    Initial branch commit. Removed redundant frontend files and reorganized the structure.

--- frontend/frontendApps/eslint.config.js
+++ frontend/frontendApps/eslint.config.js
@@ -1,55 +1,28 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactPlugin from 'eslint-plugin-react';
-import reactHooksPlugin from 'eslint-plugin-react-hooks';
-import reactRefreshPlugin from 'eslint-plugin-react-refresh';
-import prettierPlugin from 'eslint-plugin-prettier';
-import tsPlugin from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
 
-export default [
+export default tseslint.config(
+    { ignores: ["dist"] },
     {
-        ignores: ['dist'],
-    },
-    {
-        files: ['**/*.{js,jsx,ts,tsx}'],
+        extends: [js.configs.recommended, ...tseslint.configs.recommended],
+        files: ["**/*.{ts,tsx}"],
         languageOptions: {
-            parser: tsParser,
-            parserOptions: {
-                ecmaVersion: 2021,
-                sourceType: 'module',
-                ecmaFeatures: {
-                    jsx: true,
-                },
-            },
-            globals: {
-                ...globals.browser,
-                ...globals.node,
-            },
+            ecmaVersion: 2020,
+            globals: globals.browser,
         },
         plugins: {
-            react: reactPlugin,
-            'react-hooks': reactHooksPlugin,
-            'react-refresh': reactRefreshPlugin,
-            prettier: prettierPlugin,
-            '@typescript-eslint': tsPlugin,
+            "react-hooks": reactHooks,
+            "react-refresh": reactRefresh,
         },
         rules: {
-            ...js.configs.recommended.rules,
-            ...tsPlugin.configs.recommended.rules,
-            ...reactPlugin.configs.recommended.rules,
-            ...reactHooksPlugin.configs.recommended.rules,
-            'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-            'prettier/prettier': 'error',
-            'react/prop-types': 'off',
-            'no-console': 'warn',
-            'no-unused-vars': 'warn',
-            'react/react-in-jsx-scope': 'off',
+            "react/jsx-no-target-blank": "off",
+            "react/prop-types": "off",
+            "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+            "no-unused-vars": "off",
+            node: true,
         },
-        settings: {
-            react: {
-                version: 'detect',
-            },
-        },
-    },
-];
+    }
+);
