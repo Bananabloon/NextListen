0f105f60c306831e66b4ed8f23785a1a0c047746
TeKayy <th3tk.idk@gmail.com>
2025-05-28 14:53:24 +0200

    Resolved some issues with the package.json scripts.

--- frontend/webApp/vite.config.ts
+++ frontend/webApp/vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '', '');
+    const env = loadEnv(mode, "", "");
     return {
         plugins: [react()],
         server: {
+            host: "0.0.0.0",
             strictPort: true,
             port: 5173,
-            allowedHosts: [env.VITE_NGROK_URL.replace('https://', '').replace('http://', '')],
+            allowedHosts: [env.VITE_NGROK_URL.replace("https://", "").replace("http://", "")],
         },
     };
 });
