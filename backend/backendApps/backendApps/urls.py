0a62844d7b8ba55523ce9b484862a50d99b24e74
TeKayy <th3tk.idk@gmail.com>
2025-05-29 13:21:04 +0200

    Introduced path normalization to handle weird django redirect behaviour.

--- backend/backendApps/backendApps/urls.py
+++ backend/backendApps/backendApps/urls.py
@@ -23,13 +23,13 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("api/admin/", admin.site.urls),
     path("", views.homepage),
-    path("auth/", include("authentication.urls")),
-    path("about/", views.about),
-    path("users/", include("users.urls")),
-    path("spotify/", include("spotifyData.urls")),
-    path("songs/", include("songs.urls")),
+    path("api/auth/", include("authentication.urls")),
+    path("api/about/", views.about),
+    path("api/users/", include("users.urls")),
+    path("api/spotify/", include("spotifyData.urls")),
+    path("api/songs/", include("songs.urls")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- frontend/webApp/src/utils/api.ts
+++ frontend/webApp/src/utils/api.ts
@@ -1,1 +1,6 @@
-export const normalizePath = (path = "") => (path.startsWith("/") ? path : `/${path}`);
+import { trim } from "lodash";
+
+// ! important to have a slash at the end because DJANGO
+// django by default forwards requests without slash at the end
+// and it messes up with nginx and omg im so tired already
+export const normalizePath = (path: string = "") => `/${trim(path, "/")}/`;
